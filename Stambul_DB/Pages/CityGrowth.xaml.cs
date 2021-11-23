7753e7b0f8f8b99a225a6403d0c8ec5d277bf0a6
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-11-23 19:15:10 +0300

    ii

--- Stambul_DB/Pages/CityGrowth.xaml.cs
+++ Stambul_DB/Pages/CityGrowth.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Stambul_DB.db;
+using Stambul_DB.Pages;
 
 namespace Stambul_DB.Pages
 {
@@ -19,9 +21,72 @@ namespace Stambul_DB.Pages
     /// </summary>
     public partial class CityGrowth : Window
     {
+        public static Istanbul_urban_dbEntities dbEntities = new Istanbul_urban_dbEntities();
         public CityGrowth()
         {
             InitializeComponent();
+            dbEntities = new Istanbul_urban_dbEntities();
+
+            City_Growth.ItemsSource = dbEntities.City_Growth.ToList();
+        }
+        private void ButtonAdd(object sender, RoutedEventArgs e)
+        {
+
+            var q = City_Growth.SelectedItem as City_Growth;
+            if (q == null)
+            {
+                MessageBox.Show("Эта строка пуста.");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите сохранить?", "Сохранить?", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    dbEntities.City_Growth.Add(q);
+                    dbEntities.SaveChanges();
+                    City_Growth.ItemsSource = dbEntities.City_Growth.ToList();
+                }
+                catch
+                {
+                    MessageBox.Show("Удалите соединения связанные с этим данным или таких данных нет");
+                }
+
+            }
+        }
+
+        private void ButtonDel(object sender, RoutedEventArgs e)
+        {
+            var q = City_Growth.SelectedItem as City_Growth;
+            if (q == null)
+            {
+                MessageBox.Show("Эта строка и так пустая.");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите удалить строку?", "Удалить?", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    dbEntities.City_Growth.Remove(q);
+                    dbEntities.SaveChanges();
+                    City_Growth.ItemsSource = dbEntities.City_Growth.ToList();
+                }
+                catch
+                {
+                    MessageBox.Show("Удалите соединения связанные с этим данным");
+                }
+
+            }
+
+        }
+
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Redirection r = new Redirection();
+            this.Close();
+            r.Show();
         }
     }
 }
--- Stambul_DB/Pages/MasterPlan.xaml.cs
+++ Stambul_DB/Pages/MasterPlan.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Stambul_DB.db;
+using Stambul_DB.Pages;
 
 namespace Stambul_DB.Pages
 {
@@ -19,9 +21,73 @@ namespace Stambul_DB.Pages
     /// </summary>
     public partial class MasterPlan : Window
     {
+        public static Istanbul_urban_dbEntities dbEntities = new Istanbul_urban_dbEntities();
         public MasterPlan()
         {
             InitializeComponent();
+            dbEntities = new Istanbul_urban_dbEntities();
+
+            Master_Plan.ItemsSource = dbEntities.Master_Plans.ToList();
+        }
+
+        private void ButtonAdd(object sender, RoutedEventArgs e)
+        {
+
+            var q = Master_Plan.SelectedItem as Master_Plans;
+            if (q == null)
+            {
+                MessageBox.Show("Эта строка пуста.");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите сохранить?", "Сохранить?", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    dbEntities.Master_Plans.Add(q);
+                    dbEntities.SaveChanges();
+                    Master_Plan.ItemsSource = dbEntities.Master_Plans.ToList();
+                }
+                catch
+                {
+                    MessageBox.Show("Удалите соединения связанные с этим данным или таких данных нет");
+                }
+
+            }
+        }
+
+        private void ButtonDel(object sender, RoutedEventArgs e)
+        {
+            var q = Master_Plan.SelectedItem as Master_Plans;
+            if (q == null)
+            {
+                MessageBox.Show("Эта строка и так пустая.");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите удалить строку?", "Удалить?", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    dbEntities.Master_Plans.Remove(q);
+                    dbEntities.SaveChanges();
+                    Master_Plan.ItemsSource = dbEntities.Master_Plans.ToList();
+                }
+                catch
+                {
+                    MessageBox.Show("Удалите соединения связанные с этим данным");
+                }
+
+            }
+
+        }
+
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Redirection r = new Redirection();
+            this.Close();
+            r.Show();
         }
     }
 }
